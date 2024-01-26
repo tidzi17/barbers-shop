7616d8517601e1f985fef7c760945b4b3a26223e
tidzi17 <tidzi15110@gmail.com>
2024-01-26 22:13:39 +0100

    gallery res

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -61,16 +61,16 @@ const ImageGallery = ({ galleryImages }) => {
 
     
     return(
-        <div className='px-20 py-20 md1000:px-10 '>
+        <div className='lg:px-20 lg:py-20 md:px-10 py-10'>
             {openModal && 
             <div className='fixed h-screen w-screen  bg-black/50 backdrop-blur-sm top-0 right-0 z-40 scroll-m-0 flex items-center justify-center'
             onTouchStart={handleTouchStart}
             onTouchEnd={handleTouchEnd} >
             
             <RxCross1  className='top-[40px] right-[80px] text-3xl text-white fixed cursor-pointer  z-50 hover:opacity-100' onClick={handleCloseModal}/>
-            <BsChevronLeft className='top-[50%] transform -translate-y-[50%] left-[40px] text-5xl text-white fixed cursor-pointer  z-50 hover:opacity-100' onClick={prevSlide}/>
-            <BsChevronCompactRight className='top-[50%] transform -translate-y-[50%] right-[40px]  text-5xl text-white fixed cursor-pointer  z-50 hover:opacity-100' onClick={nextSlide} />
-            <div className='flex items-center justify-center w-full h-full'>
+            <BsChevronLeft className='top-[50%] transform -translate-y-[50%] left-0 md:left-[40px] text-5xl text-white fixed cursor-pointer  z-50 hover:opacity-100' onClick={prevSlide}/>
+            <BsChevronCompactRight className='top-[50%] transform -translate-y-[50%] right-0 md:right-[40px]  text-5xl text-white fixed cursor-pointer  z-50 hover:opacity-100' onClick={nextSlide} />
+            <div className='flex items-center justify-center w-[80%] md:w-full h-full'>
                 <img src={galleryImages[slideNumber]} alt='' className='grayscale max-w-full max-h-full pointer-events-none select-none'/>
             </div>
                 </div>
--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -12,9 +12,9 @@ export default function Gallery(){
          <div>
         <img src={landingimage} alt="landingimage" className="w-full h-[70vh] object-cover grayscale relative"/>
         <div className="w-full absolute top-0 text-white flex flex-col items-center transform translate-y-64 gap-5 ">
-            <p className='font-alumni text-6xl uppercase '>Gallery</p>
+            <p className='font-alumni text-4xl md:text-6xl uppercase '>Gallery</p>
             <div className='w-[50px] h-[2px] bg-white'></div>
-            <Link to="/" className='font-mono uppercase text-2xl'>Home</Link>
+            <Link to="/" className='font-mono uppercase text-xl md:text-2xl'>Home</Link>
         </div>
         </div>
         <ImageGallery galleryImages={galleryImages} />
