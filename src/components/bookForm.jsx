6242141c986fe0e4f0dac823d950d58a2aa151ac
tidzi17 <tidzi15110@gmail.com>
2024-01-30 20:26:00 +0100

    finished bookForm

--- src/components/bookForm.jsx
+++ src/components/bookForm.jsx
@@ -15,7 +15,7 @@
         const [submittedValues, setSubmittedValues] = useState(null);
         const messageBoxRef = useRef(null);
 
-        const {values, errors, touched, isSubmitting,  handleChange, handleSubmit} = useFormik({
+        const {values, errors, touched, isSubmitting, handleBlur,  handleChange, handleSubmit} = useFormik({
             initialValues: {
             date: "",
             time: "",
@@ -37,30 +37,51 @@
             
         };
 
-      
+        const timeOptions = [
+            "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30",
+            "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30",
+            "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30",
+            "19:00", "19:30", "20:00", "20:30"
+          ];
 
-        
+  
 
+         
+            
         return(
             <div className="w-full h-full">
                 <form onSubmit={handleFormSubmit}  autoComplete="off" className="flex flex-col items-center w-full gap-6">
                 <div className="flex gap-3 w-full h-12">
-
-                    <div className={`w-1/2  pl-3  h-full  border-[1px]  ${errors.date && touched.date ? "border-red-500" : "border-zinc-300"} relative`}>
+                    <div className={`w-1/2  pl-3   h-full  border-[1px]  ${errors.date && touched.date ? "border-red-500" : "border-zinc-300"} relative`}>
                     <input 
                     value={values.date}
                     onChange={handleChange}
-                    type="date" placeholder="Date" id="date"
+                    onBlur={handleBlur}
+                    type="date" 
+                    placeholder="Date"
+                     id="date"
+                     min={new Date().toISOString().split("T")[0]}
                     className="w-full h-full bg-transparent  placeholder:text-neutral-700 placeholder:pl-3 outline-none"></input>
                     {errors.date && touched.date && <p className="absolute bottom-[-25px] text-red-500 font-sans font-light text-sm">{errors.date}</p>}
                     </div>
 
-                    <div className={`w-1/2  h-full  pl-3  border-[1px]  ${errors.time && touched.time ? "border-red-500" : "border-zinc-300"} relative`}>
-                    <input value={values.time}
+                    <div className={`w-1/2  h-full   pl-3  border-[1px]  ${errors.time && touched.time ? "border-red-500" : "border-zinc-300"} relative`}>
+                    <select
+                     value={values.time}
                     onChange={handleChange}
+                    onBlur={handleBlur}
                     type="time" min="08:00" max="20:00" 
                     placeholder="Time" id="time" 
-                    className="w-full h-full bg-transparent  placeholder:text-neutral-700 placeholder:pl-3 outline-none"></input>
+                    className="w-full h-full bg-transparent  outline-none">
+                         <option value="" disabled selected className="text-neutral-700">
+                                    Time
+                                </option>
+                        {timeOptions.map((time, index) => (
+                        <option key={index} value={time} className="bg-black">
+                               {time}
+                              </option>
+                            ))}
+                    </select>
                     {errors.time && touched.time && <p className="absolute bottom-[-25px] text-red-500 font-sans font-light text-sm">{errors.time}</p>}
                     </div>
                 </div>
@@ -69,12 +90,12 @@
                     <input
                         value={values.name}
                         onChange={handleChange}
+                        onBlur={handleBlur}
                         type="text"
                         name="name"
                         id="name"
                         placeholder="Your first & last name"
-                        className={`w-full h-full bg-transparent  placeholder-pl-3 placeholder:text-neutral-700 outline-none `}
-                    />
+                        className="w-full h-full bg-transparent  placeholder-pl-3 placeholder:text-neutral-700 outline-none"/>
                     {errors.name && touched.name && <p className="absolute bottom-[-25px] text-red-500 font-sans font-light text-sm">{errors.name}</p>}
                     </div>
 
@@ -82,6 +103,7 @@
                 <input
                 value={values.contact} 
                 onChange={handleChange} 
+                onBlur={handleBlur}
                 type="tel" name="contact" id="contact"
                 placeholder="Your Phone Number" 
                 className="w-full h-full bg-transparent  placeholder:text-neutral-700 outline-none"/>
@@ -98,6 +120,9 @@
                 
                 <button  disabled={isSubmitting}  type="submit" className="font-mono text-base bg-zinc-800 text-zinc-400 px-4 py-2">Book Apointment</button>
                 </form>
+
+
+
                 {isSubmitted && (
                <div ref={messageBoxRef} className="fixed top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm z-30 flex justify-center items-center">
                <div className="w-96 h-[500px] p-8 bg-zinc-900 shadow-zinc-800 shadow-sm rounded-lg ">
--- src/components/formSchema.jsx
+++ src/components/formSchema.jsx
@@ -6,8 +6,12 @@ export const formSchema = yup.object().shape({
     name: yup.string().matches(/^[a-zA-Z]+ [a-zA-Z]+$/, 'Please enter your first and last name').required("First and Last names are required"),
     contact: yup.string().matches(phoneRegExp, "Phone number is not valid").required("Contact number is required"),
     date: yup.date().required("Date is required"),
-   /*  time: yup.date().required("Time is required"), */
-  
-    
-
+    time: yup.string()
+    .test('valid-time', 'Invalid time selection', (value) => {
+      if (!value) return false;
+      const time = value.split(':').map(Number);
+      const [hours, minutes] = time;
+      return hours >= 7 && hours <= 20 && minutes % 30 === 0;
+    })
+    .required("Time is required"),
 });
